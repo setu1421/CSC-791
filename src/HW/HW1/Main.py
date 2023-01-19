1460da7cbf2e4c904aedbfa6e70303c66918c6b7
Setu Kumar Basak <setu.1421@yahoo.com>
2023-01-19 13:24:01 -0500

    Fix error in Rand function

--- src/HW/HW1/Main.py
+++ src/HW/HW1/Main.py
@@ -6,15 +6,17 @@ if __name__ == "__main__":
     # Test 1
     test_list1 = [1, 1, 1, 1, 2, 2, 3]
     test_status = TestEngine.RunTest(test_list1, "test1")
-    if test_status: 
+    if test_status == False: 
         fails = fails + 1
     # Test 2
     test_list2 = ["a", "a", "a", "a", "b", "b", "c"] 
-    if TestEngine.RunTest(test_list2, "test2") == False: 
+    test_status = TestEngine.RunTest(test_list2, "test2")
+    if test_status == False: 
         fails = fails + 1
     # Test 3
     test_seed = 937162211
-    if TestEngine.RunTest(test_seed, "test3") == False:
+    test_status = TestEngine.RunTest(test_seed, "test3")
+    if test_status == False:
         fails = fails + 1    
 
     
--- src/HW/HW1/TestEngine.py
+++ src/HW/HW1/TestEngine.py
@@ -18,15 +18,15 @@ class TestEngine:
         elif(test_no == "test3"):
             num1 = NUM()
             num2 = NUM()
-            rand1 = Rand()
+            rand1 = Rand(input)
             for i in range(1, 1000):
                 num1.Add(rand1.rand(0,1)) 
-            rand2 = Rand()
+            rand2 = Rand(input)
             for i in range(1, 1000):
                 num2.Add(rand2.rand(0,1))     
             m1 = Rand.rnd(num1.Div(), 10)
             m2 = Rand.rnd(num2.Div(), 10)        
 
-            return (m1 == m2) and (0.5 == Rand.rand(m1, 1))
+            return (m1 == m2) and (0.3 == Rand.rnd(m1, 1))
 
 
