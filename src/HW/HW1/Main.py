1fab81f97a3ceaa7ead8ccabf784eb4cbebd37ec
Setu Kumar Basak <setu.1421@yahoo.com>
2023-01-19 11:22:26 -0500

    Testing Exit codes

--- src/HW/HW1/Main.py
+++ src/HW/HW1/Main.py
@@ -1,7 +1,10 @@
 from TestEngine import TestEngine
+import sys
 
 if __name__ == "__main__":    
     test_list1 = [1, 1, 1, 1, 2, 2, 3]
     print(TestEngine.RunTest(test_list1, "num"))
     test_list2 = ["a", "a", "a", "a", "b", "b", "c"] 
     print(TestEngine.RunTest(test_list2, "sym"))
+    
+    sys.exit(1)
